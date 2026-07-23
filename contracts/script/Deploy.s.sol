42f200509793d8dc4d5a76d0e0e6da62ad88cddc
Vishnu Govind <90692854+vishnugovind10@users.noreply.github.com>
2026-07-23 15:17:51 +0200

    fix(contracts): avoid deploy script stack pressure

--- contracts/script/Deploy.s.sol
+++ contracts/script/Deploy.s.sol
@@ -13,42 +13,68 @@ contract Deploy is Script {
     uint256 internal constant INVESTOR_CASH = 500_000;
     uint256 internal constant UNDERFUNDED_CASH = 1_000;
 
-    function run() external {
-        address issuer = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
-        address investorA = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
-        address investorB = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
-        address investorC = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
-        address investorD = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
-        address investorE = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
+    address internal constant ISSUER = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266;
+    address internal constant INVESTOR_A = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8;
+    address internal constant INVESTOR_B = 0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC;
+    address internal constant INVESTOR_C = 0x90F79bf6EB2c4f870365E785982E1f101E93b906;
+    address internal constant INVESTOR_D = 0x15d34AAf54267DB7D7c367839AAf71A00a2C6A65;
+    address internal constant INVESTOR_E = 0x9965507D1a55bcC2695C58ba16FB37d819B0A4dc;
 
+    function run() external {
         vm.startBroadcast();
         ComplianceRegistry registry = new ComplianceRegistry();
-        RestrictedAssetToken asset = new RestrictedAssetToken(issuer, SUPPLY, registry);
-        CashToken cash = new CashToken(issuer, SUPPLY * 5);
+        RestrictedAssetToken asset = new RestrictedAssetToken(ISSUER, SUPPLY, registry);
+        CashToken cash = new CashToken(ISSUER, SUPPLY * 5);
         DvPEscrow escrow = new DvPEscrow(asset, cash);
         CouponDistributor distributor = new CouponDistributor(cash, asset);
 
-        registry.setAllowlisted(issuer, true);
-        registry.setAllowlisted(investorA, true);
-        registry.setAllowlisted(investorB, true);
-        registry.setAllowlisted(investorC, true);
-        registry.setAllowlisted(investorD, true);
-        registry.setAllowlisted(investorE, true);
+        registry.setAllowlisted(ISSUER, true);
+        registry.setAllowlisted(INVESTOR_A, true);
+        registry.setAllowlisted(INVESTOR_B, true);
+        registry.setAllowlisted(INVESTOR_C, true);
+        registry.setAllowlisted(INVESTOR_D, true);
+        registry.setAllowlisted(INVESTOR_E, true);
         registry.setAllowlisted(address(escrow), true);
         registry.setAllowlisted(address(distributor), true);
-        registry.setLockup(investorD, block.timestamp + 7 days);
+        registry.setLockup(INVESTOR_D, block.timestamp + 7 days);
 
-        cash.transfer(investorA, INVESTOR_CASH);
-        cash.transfer(investorB, INVESTOR_CASH);
-        cash.transfer(investorC, INVESTOR_CASH);
-        cash.transfer(investorD, INVESTOR_CASH);
-        cash.transfer(investorE, UNDERFUNDED_CASH);
+        cash.transfer(INVESTOR_A, INVESTOR_CASH);
+        cash.transfer(INVESTOR_B, INVESTOR_CASH);
+        cash.transfer(INVESTOR_C, INVESTOR_CASH);
+        cash.transfer(INVESTOR_D, INVESTOR_CASH);
+        cash.transfer(INVESTOR_E, UNDERFUNDED_CASH);
         vm.stopBroadcast();
 
-        string memory json = string.concat(
+        vm.writeJson(deploymentJson(registry, asset, cash, escrow, distributor), "out/deployment.json");
+    }
+
+    function deploymentJson(
+        ComplianceRegistry registry,
+        RestrictedAssetToken asset,
+        CashToken cash,
+        DvPEscrow escrow,
+        CouponDistributor distributor
+    ) internal view returns (string memory) {
+        return string.concat(
             '{"chain_id":',
             vm.toString(block.chainid),
-            ',"rpc_url":"http://anvil:8545","addresses":{',
+            ',"rpc_url":"http://anvil:8545","addresses":',
+            addressesJson(registry, asset, cash, escrow, distributor),
+            ',"personas":',
+            personasJson(),
+            "}"
+        );
+    }
+
+    function addressesJson(
+        ComplianceRegistry registry,
+        RestrictedAssetToken asset,
+        CashToken cash,
+        DvPEscrow escrow,
+        CouponDistributor distributor
+    ) internal view returns (string memory) {
+        return string.concat(
+            "{",
             '"ComplianceRegistry":"',
             vm.toString(address(registry)),
             '","RestrictedAssetToken":"',
@@ -59,21 +85,26 @@ contract Deploy is Script {
             vm.toString(address(escrow)),
             '","CouponDistributor":"',
             vm.toString(address(distributor)),
-            '"},"personas":{',
-            '"issuer":"',
-            vm.toString(issuer),
-            '","investor-a":"',
-            vm.toString(investorA),
-            '","investor-b":"',
-            vm.toString(investorB),
-            '","investor-c":"',
-            vm.toString(investorC),
-            '","investor-d":"',
-            vm.toString(investorD),
-            '","investor-e":"',
-            vm.toString(investorE),
-            '"}}'
+            '"}'
         );
-        vm.writeJson(json, "out/deployment.json");
+    }
+
+    function personasJson() internal view returns (string memory) {
+        return string.concat(
+            "{",
+            '"issuer":"',
+            vm.toString(ISSUER),
+            '","investor-a":"',
+            vm.toString(INVESTOR_A),
+            '","investor-b":"',
+            vm.toString(INVESTOR_B),
+            '","investor-c":"',
+            vm.toString(INVESTOR_C),
+            '","investor-d":"',
+            vm.toString(INVESTOR_D),
+            '","investor-e":"',
+            vm.toString(INVESTOR_E),
+            '"}'
+        );
     }
 }
